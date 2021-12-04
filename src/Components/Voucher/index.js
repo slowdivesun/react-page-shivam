c920b0b0c29f638f08a13395eb1463a0bff7ea21
Shivam <shivam.gup82@gmail.com>
2021-12-05 03:56:38 +0530

    added all

---
+++ src/Components/Voucher/index.js
@@ -0,0 +1,11 @@
+import "./index.css"
+const Vouch = () => {
+    return (
+        <div className="vouch-container">
+            <input type="text" class="code" placeholder="Voucher Code"/>
+            <p className="button">Apply</p>
+        </div>
+    )
+}
+
+export default Vouch
